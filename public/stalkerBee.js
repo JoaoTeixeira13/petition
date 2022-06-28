1d53bbb4195965c6a069f8fa6ac64c2e34468892
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-06-28 20:01:54 +0200

    berlin insects ready to deploy

--- public/stalkerBee.js
+++ public/stalkerBee.js
@@ -35,19 +35,3 @@ function delayStalker() {
     stalker.style.left = revisedBeeX + "px";
 }
 delayStalker();
-
-//picture frames
-
-let frames = false;
-
-let picFrames = document.querySelectorAll(".form-img");
-
-if (picFrames.length > 0) {
-    picFrames[0].addEventListener("mouseover", () => {
-        picFrames[0].style.border = "10px solid #fda500";
-    });
-
-    picFrames[0].addEventListener("mouseout", () => {
-        picFrames[0].style.border = "10px solid #f1e4e4";
-    });
-}
